e1d8b9b32b6de37f4071afdc76536819288886d2
Benarnie <89978660+Benarnie@users.noreply.github.com>
2022-01-04 12:20:42 +0000

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -1,4 +1,40 @@
-function start() {
-
-let gameScreen = 
+function startGame()
+{
+    document.getElementById('intro').style.display = 'none';
+    document.getElementById('game').style.display = 'block';
 }
+
+function incrementValue()
+    {
+        var value = parseInt(document.getElementById('number').value, 10);
+        value = isNaN(value) ? 0 : value;
+        value++;
+        document.getElementById('number').value = value;
+    }
+
+function fizzbuzz()
+    {
+        var value = document.getElementById('number').value
+
+        if (value % 3 === 0 && value % 5 === 0) 
+            incrementValue()
+        else (value === "Game Over!")
+    }
+
+function fizz()
+    {
+        var value = document.getElementById('number').value
+
+        if (value % 3 === 0) 
+            incrementValue()
+        else (value === "Game Over!")
+    }
+
+function buzz()
+    {
+        var value = document.getElementById('number').value
+
+        if (value % 5 === 0) 
+            incrementValue()
+        else (value === "Game Over!")
+    }
