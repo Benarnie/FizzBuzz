91f33eb5a3e31139279bd48573c09d6fcfb42bd7
Benarnie <89978660+Benarnie@users.noreply.github.com>
2022-01-07 00:24:17 +0000

    Update myscripts.js
    Game basics complete

--- myscripts.js
+++ myscripts.js
@@ -1,3 +1,12 @@
+var value = document.getElementById('number').value
+var fizzz = document.getElementById('fizz')                     //A variant and a function cant have the same name
+var buzzz = document.getElementById('buzz')
+var fizzBuzz = document.getElementById('fizzBuzz')
+var introDisplay = document.getElementById('intro').style.display
+var gameDisplay = document.getElementById('game').style.display
+
+
+
 function startGame()
 {
     document.getElementById('intro').style.display = 'none';
@@ -9,32 +18,56 @@ function incrementValue()
         var value = parseInt(document.getElementById('number').value, 10);
         value = isNaN(value) ? 0 : value;
         value++;
-        document.getElementById('number').value = value;
+        document.getElementById('number').value = value
+    }
+
+function gameOver()
+    {
+        document.getElementById('number').value = value     
+        value = "Game Over!";
     }
 
 function fizzbuzz()
     {
         var value = document.getElementById('number').value
 
-        if (value % 3 === 0 && value % 5 === 0) 
-            incrementValue()
-        else (value === "Game Over!")
+        if (value % 3 === 0 && value % 5 === 0) {
+            incrementValue()            
+        }   else {
+            gameOver()   
+        }
     }
 
 function fizz()
     {
         var value = document.getElementById('number').value
 
-        if (value % 3 === 0) 
+        if (value % 3 === 0) {
             incrementValue()
-        else (value === "Game Over!")
+        }   else {
+            gameOver()
+        }
     }
 
 function buzz()
     {
         var value = document.getElementById('number').value
 
-        if (value % 5 === 0) 
+        if (value % 5 === 0) {
             incrementValue()
-        else (value === "Game Over!")
+        }   else {
+            gameOver()
+        }
     }
+
+function incByOne()
+    {
+        var value = document.getElementById('number').value
+
+        if (value % 3 === 0 || value % 5 === 0) {
+            gameOver()
+        }   else (
+            incrementValue()
+        )
+    }
+
